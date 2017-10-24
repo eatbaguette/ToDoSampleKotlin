fd17398267981d5a8b4a2a43c123124edc152b9a
monkey <gikeda@appirits.com>
2017-10-24 12:54:11 +0900

    [add] Realmを実装

--- app/src/main/java/com/list_sample/todosample/ui/MainActivity.kt
+++ app/src/main/java/com/list_sample/todosample/ui/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.list_sample.todosample.ui
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.widget.EditText
 import android.widget.Toast
 import com.list_sample.todosample.R
 import com.list_sample.todosample.adapter.RecyclerViewAdapter
@@ -19,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var mRealm: Realm
     lateinit var mRecyclerView: RecyclerView
     lateinit var mAdapter: RecyclerViewAdapter
+    lateinit var dateList: RealmResults<TodoModel>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         mRealm = Realm.getInstance(realmConfig)
 
         // Realm読み込み
-        val dateList: RealmResults<TodoModel> = mRealm.where(TodoModel::class.java).findAll()
+        this.dateList = mRealm.where(TodoModel::class.java).findAll()
 
         // RecyclerViewのセットアップ
         mRecyclerView = findViewById(R.id.recyclerView_activity_main) as RecyclerView
@@ -46,7 +51,24 @@ class MainActivity : AppCompatActivity() {
         mRecyclerView.addItemDecoration(DividerItemDecoration(this, 1))
 
         fab_activity_main.setOnClickListener {
-            Toast.makeText(this, "hoge", Toast.LENGTH_LONG).show()
+            showEditTextDialog(mRealm)
         }
+    }
+
+    private fun showEditTextDialog(mRealm: Realm) {
+        val editText = EditText(this)
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle(R.string.alert_dialog_title)
+        dialog.setView(editText)
+        dialog.setPositiveButton(R.string.alert_dialog_ok, DialogInterface.OnClickListener { dialogInterface, i ->
+            mRealm.executeTransaction {
+                val todoModel = this.mRealm.createObject(TodoModel::class.java)
+                todoModel.todo = editText?.text.toString()
+                mRealm.copyToRealm(todoModel)
+            }
+            mAdapter.notifyDataSetChanged()
+        })
+
+        dialog.show()
     }
 }
